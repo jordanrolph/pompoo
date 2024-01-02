9782e08ecc54027227db7f1e2089119cdc25e049
Jordan <jordan.rolph@gmail.com>
2024-01-02 12:13:53 +0000

    Add Vercel speed insights

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { type AppType } from "next/app";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
@@ -9,6 +10,7 @@ const MyApp: AppType = ({ Component, pageProps }) => {
     <>
       <Component {...pageProps} />
       <Analytics />
+      <SpeedInsights />
     </>
   );
 };
